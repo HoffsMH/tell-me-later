595cc2ecd0b99baa69de1e13c2b12e8ef30cd109
HoffsMH <hecker.matthew@gmail.com>
2016-02-27 15:23:41 -0700

    creates model and table for Lists

---
+++ db/migrate/20160227220929_create_basic_list.rb
@@ -0,0 +1,12 @@
+class CreateBasicList < ActiveRecord::Migration
+  def change
+    create_table :lists do |t|
+      t.string :code, index: true
+
+      t.integer :item_count
+      t.datetime :last_changed
+
+      t.timestamps null: false
+    end
+  end
+end
