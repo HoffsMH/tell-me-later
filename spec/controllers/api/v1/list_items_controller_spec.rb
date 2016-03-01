c652730efed6ff6f1f0040b01939b116efb1a9a1
HoffsMH <hecker.matthew@gmail.com>
2016-03-01 15:24:16 -0700

    adds first test for creating a new list item

--- spec/controllers/api/v1/list_items_controller_spec.rb
+++ spec/controllers/api/v1/list_items_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ListItemsController, type: :controller do
 
+  describe 'POST #create' do
+    context 'when parameters are correct' do
+      let!(:valid_list_item) do
+        {
+          list_id:  'valid list id',
+          priority: 5,
+          title: "valid title",
+          content: "valid content",
+          show_time: "Tue Mar 01 2016 15:20:18 GMT-0700 (MST)"
+        }
+      end
+      context 'and no list exists for provided list id' do
+        it 'creates a new list and returns json form of the new list item' do
+          post :create, valid_list_item
+          # expect(response).to
+          binding.pry
+        end
+      end
+    end
+  end
 end
