efc49a6dce54893a7d7f2caaf2cb120def7b572b
HoffsMH <hecker.matthew@gmail.com>
2016-02-29 18:59:37 -0700

    adds preliminary scaffolding to how list handler class will work

--- app/models/list_handler.rb
+++ app/models/list_handler.rb
@@ -1,3 +1,19 @@
 class ListHandler
-  
+  def self.create_item(params)
+    if params[:code]
+      return add_item_to_list(params)
+    else
+      params[:code] = make_new_list
+      return add_item_to_list
+    end
+  end
+
+  def add_item_to_list(params)
+    list_item = ListItem.new(params)
+    if list_item.save
+      return list_item
+    else
+      return list_item.errors.full_messages
+    end
+  end
 end
