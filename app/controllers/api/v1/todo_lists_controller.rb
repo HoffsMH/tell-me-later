84a8d75cb322fdaea341ab0bad6e95ffc921a040
HoffsMH <hecker.matthew@gmail.com>
2016-03-07 00:13:47 -0700

    todo_list controller now has standardized json output

--- app/controllers/api/v1/todo_lists_controller.rb
+++ app/controllers/api/v1/todo_lists_controller.rb
@@ -4,9 +4,19 @@ class Api::V1::TodoListsController < ApplicationController
   def show
     todo_list = TodoList.find_by(code: todo_list_params)
     if todo_list
-      render json: {todo_list: todo_list.todo_items.as_json}
+      output = TodoResponder
+        .groom_response(code: 204, 
+                        resource:
+                          {todo_list: todo_list.todo_items.as_json})
+
+      render json: output.as_json, status: 201
     else
-      render json: {todo_list: "todo_list not found"}
+      output = TodoResponder
+        .groom_response(code: 404,
+                        resource: nil,
+                        message: "todo_list not found")
+
+      render json: output, status: 404
     end
   end
 
